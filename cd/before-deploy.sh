f9ebc7dfa0354e9f1db0df46d6eadc91f349321f
Isen Ng <isen@treeboxsolutions.com>
2018-08-23 16:49:01 +0800

    Fix CI deployment by using RSA keys

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_6c3b7719f7ba_key -iv $encrypted_6c3b7719f7ba_iv -in codesigning.asc.enc -out codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_fb3952287f03_key -iv $encrypted_fb3952287f03_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
