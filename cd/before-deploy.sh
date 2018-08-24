e7eda702ca305474c6776475144299c92da2dab0
Isen Ng <isen@treeboxsolutions.com>
2018-08-24 12:59:41 +0800

    Fix CI to only run on tags

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+if [ -z "$TRAVIS_TAG" ]; then exit 0; fi
+
 openssl aes-256-cbc -K $encrypted_fb3952287f03_key -iv $encrypted_fb3952287f03_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 gpg --fast-import cd/codesigning.asc
--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+if [ -z "$TRAVIS_TAG" ]; then exit 0; fi
+
 mvn deploy -P sign,generate-docs --settings cd/mvnsettings.xml
